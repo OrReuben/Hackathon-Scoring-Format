609b03ec39bea4c96be9dcb2c6ad0ae155867134
OrReuben <orreuven1243@gmail.com>
2023-02-14 20:11:37 +0200

    added header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import cyberpro from "../assets/Cyberpro.png";
+import atidim from "../assets/lohamim_lahaytek.png";
+import './Header.css'
 const Header = () => {
   return (
-    <div>
-        <img src='../assets/Cyberpro.png' alt="cyberpro" />
-        <h2>Hackathon - November</h2>
-        <img src='' alt="atidim" />
+    <div className="header-container">
+      <img src={cyberpro} alt="cyberpro" />
+      <h1>Hackathon - November</h1>
+      <img src={atidim} alt="atidim" />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
