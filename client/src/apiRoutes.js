ecbf5a2c1632f8dcfd43dabb1153588a508bf903
or reuben <orreuven1243@gmail.com>
2023-06-14 22:05:46 +0300

    hackathon no footer

--- client/src/apiRoutes.js
+++ client/src/apiRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000"
+export const host = "https://hackathon-scoring-api.onrender.com"
 export const getScores = `${host}/scores`
 export const deleteScores = `${host}/scores/reset`
 export const updateScores = `${host}/update-scores`
